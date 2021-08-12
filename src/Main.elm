b9a1dd37fbb5416fc6c7fe8c541b9985ee3f72d0
liaob88 <50452793+liaob88@users.noreply.github.com>
2021-08-12 23:00:48 +0900

    [1.2] 型注釈を加える sayHello 関数

--- src/Main.elm
+++ src/Main.elm
@@ -2,16 +2,11 @@ module Main exposing (main)
 
 import Html exposing (text)
 
-main = text ( Debug.toString meaningOfLife )
 
-greeting: String
-greeting = "Hello, Static Elm!"
+main =
+    text (sayHello "Functional Elm!")
 
-meaningOfLife : Int
-meaningOfLife = 42
 
-pi: Float
-pi = 3.4
-
-canChunk : Bool
-canChunk = True
+sayHello : String -> String
+sayHello name =
+    "Hello, " ++ name ++ "."
