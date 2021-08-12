3a2cc86370fff1fba96f648e1a7f56ffaefcb4ec
liaob88 <50452793+liaob88@users.noreply.github.com>
2021-08-12 22:52:52 +0900

    [1.2] 型注釈を加える text 関数で表示するには Debug.toString で文字列へ変換する

--- src/Main.elm
+++ src/Main.elm
@@ -2,7 +2,16 @@ module Main exposing (main)
 
 import Html exposing (text)
 
-main = text greeting
+main = text ( Debug.toString meaningOfLife )
 
 greeting: String
 greeting = "Hello, Static Elm!"
+
+meaningOfLife : Int
+meaningOfLife = 42
+
+pi: Float
+pi = 3.4
+
+canChunk : Bool
+canChunk = True
