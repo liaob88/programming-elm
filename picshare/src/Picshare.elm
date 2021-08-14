c8a27e4d02527a9f76ae6e25ec55bd9c8849d772
liaob88 <50452793+liaob88@users.noreply.github.com>
2021-08-14 12:35:28 +0900

    [1.3] 写真を表示する 複数の写真を表示できるところまで

--- picshare/src/Picshare.elm
+++ picshare/src/Picshare.elm
@@ -1,9 +1,36 @@
 module Picshare exposing (main)
 
-import Html exposing (Html, div, h1, text)
-import Html.Attributes exposing (class)
+import Html exposing (..)
+import Html.Attributes exposing (class, src)
+
 
 main : Html msg
 main =
-    div [ class "header" ] [ h1 [] [ text "Picshare" ]]    
+    div []
+        [ div [ class "header" ]
+            [ h1 [] [ text "Picshare" ] ]
+        , div [ class "content-flow" ]
+            [ viewDetailedPhoto (baseUrl ++ "1.jpg")
+                "Surfing"
+            , viewDetailedPhoto
+                (baseUrl ++ "2.jpg")
+                "The Fox"
+            , viewDetailedPhoto
+                (baseUrl ++ "3.jpg")
+                "Evening"
+            ]
+        ]
 
+
+baseUrl : String
+baseUrl =
+    "https://programming-elm.com/"
+
+
+viewDetailedPhoto : String -> String -> Html msg
+viewDetailedPhoto url caption =
+    div [ class "detailed-photo" ]
+        [ img [ src url ] []
+        , div [ class "photo-info" ]
+            [ h2 [ class "caption" ] [ text caption ] ]
+        ]
